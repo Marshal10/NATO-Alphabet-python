c011f19e3de8a529e3528d42f56206c20b1892b0
Marshal10 <marshalmaleane123@gmail.com>
2023-12-30 23:59:33 +0530

    Exception handling

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import pandas
 data=pandas.read_csv("data.csv")
 phonetic_dict={row.letter:row.code for (index,row) in data.iterrows()}
 
-user_input=input("Enter a word: ").upper()
-result=[phonetic_dict[letter] for letter in user_input]
+ask_user=True
+while ask_user: 
+    user_input=input("Enter a word: ").upper()
+    try:
+        result=[phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+    else:
+        ask_user=False
+            
+
 print(result)
